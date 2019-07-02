5465cc841da05cffde6707ed6b1cc4185748eec1
Artem <52420751+tmanxiety@users.noreply.github.com>
2019-07-02 11:53:37 +0300

    master

---
+++ src/components/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Player from './Player';
+import AddPlayerForm from './AddPlayerForm';
+
+class App extends Component {
+  state = {
+    players: [
+      {
+        name: "Daniil",
+        score: 0,
+        id: 1
+      },
+      {
+        name: "Dima",
+        score: 0,
+        id: 2
+      },
+      {
+        name: "Tema",
+        score: 0,
+        id: 3
+      },
+      {
+        name: "Vika",
+        score: 0,
+        id: 4
+      }
+    ]
+  };
+
+  // player id counter
+  prevPlayerId = 4;
+
+  getHighScore = () => {
+    const scores = this.state.players.map( p => p.score );
+    const highScore = Math.max(...scores);
+    if (highScore) {
+      return highScore;
+    } 
+    return null;
+  }
+
+  handleScoreChange = (index, delta) => {
+    this.setState( prevState => ({
+      score: prevState.players[index].score += delta
+    }));
+  }
+
+  handleAddPlayer = (name) => {
+    this.setState( prevState => {
+      return {
+        players: [
+          ...prevState.players,
+          {
+            name,
+            score: 0,
+            id: this.prevPlayerId += 1
+          }
+        ]
+      };
+    });
+  }
+
+  handleRemovePlayer = (id) => {
+    this.setState( prevState => {
+      return {
+        players: prevState.players.filter(p => p.id !== id)
+      };
+    });
+  }
+
+  render() {
+    const highScore = this.getHighScore();
+    
+    return (
+      <div className="scoreboard">
+        <Header players={this.state.players} />
+  
+        {/* Players list */}
+        {this.state.players.map( (player, index) =>
+          <Player 
+            name={player.name}
+            score={player.score}
+            id={player.id}
+            key={player.id.toString()} 
+            index={index}
+            changeScore={this.handleScoreChange}
+            removePlayer={this.handleRemovePlayer}
+            isHighScore={highScore === player.score}         
+          />
+        )}
+
+        <AddPlayerForm addPlayer={this.handleAddPlayer} />
+      </div>
+    );
+  }
+}
+
+export default App;
